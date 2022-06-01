0da18ff424cc9a9737b826cff10edf49035eb1a2
Dave Fisher <dave.j.fish@gmail.com>
2022-06-01 14:55:46 -0700

    grabbed checkbox and verified

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // grab DOM elements
 
+const tosCheckbox = document.getElementById('tosAgreement');
+
+tosCheckbox.addEventListener('change', () => {
+    console.log('checkbox checked');
+});
+
 // set event listeners
     // get info from user input
     // use user input to update state
